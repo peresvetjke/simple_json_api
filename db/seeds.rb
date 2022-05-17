d1f2eac7240b591a3faeb5a97669bb32d9059096
peresvetjke <igor.udalov@gmail.com>
2022-05-17 19:50:09 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |t|
+  FactoryBot.create(:tag,
+    name: "tag_#{t+1}",
+    slug: "url/tag_#{t+1}"
+  )
+end
+
+30.times do |t|
+  FactoryBot.create(:topic,
+    title: "material_#{t+1}", 
+    url: "path/to/material_#{t+1}",
+    publication_date: Time.now,
+    image_link: "image_link",
+    annonce: "text",
+    body: "text"
+  )
+end
+
+Topic.first(5).each do |topic|
+  topic.tag_list.add Tag.find(rand(1..5)).name
+  topic.save!
+end
